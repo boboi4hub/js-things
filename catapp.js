1fc3233dc7deb364742c885c9b5a63c03e51a3be
boboi4hub <87900712+boboi4hub@users.noreply.github.com>
2022-11-12 17:36:13 +0100

    Add files via upload

---
+++ catapp.js
@@ -0,0 +1,373 @@
+// var age = 13;
+// // if (age > 13) {
+// //   !accompanied;
+// // }
+// // else (age < 13); {
+// //     accompanied;
+// // }
+// // if (age < 13) {
+// //   accompanied = false;
+// // }
+// var x = "accompanied is " + (age < 13);
+// задача с ограничением 13 лет
+
+// масив
+// var dinosaur1 = "T-Rex";
+// var dinosaur2 = "Velociraptor";
+// var dinosaur3 = "Stegosaurus";
+// var dinosaur4 = "Triseratops";
+// var dinosaur5 = "Brachiosaurus";
+// var dinosaur6 = "Pteranodon";
+// var dinosaur7 = "Apatosaurus";
+// var dinosaur8 = "Diplodocus";
+// var dinosaur9 = "Compsognathus";
+
+// var dinosaurs = [
+//   "T-Rex",
+//   "Velociraptor",
+//   "Stegosaurus",
+//   "Triseratops",
+//   "Brachiosaurus",
+//   "Pteranodon",
+//   "Apatosaurus",
+//   "Apatosaurus",
+//   undefined,
+//   "Compsognathus",
+// ];
+// dinosaurs[0] = "Tyrannosaurus Rex";
+// dinosaurs[10] = 456;
+// dinosaurs.pop[1];
+
+// console.log(dinosaurs[0]);
+
+// var furryAnimals = ["Alpaca", "Ring-tailed Lemur", "Yeti"];
+// var scalyAnimals = ["Boa Constrictor", "Godzilla"];
+// var featheredAnimals = ["Macaw", "Dodo"];
+// var allAnimals = furryAnimals.concat(scalyAnimals, featheredAnimals);
+
+// console.log(Math.floor(Math.random() * 2));
+
+// var RandomWords = ["Explosion", "Princess", "Cave", "Pen"];
+// var RandomIndex = Math.floor(Math.random() * 4);
+
+// console.log(RandomWords[RandomIndex]);
+
+// var randomBodeParts = ["Face", "Nose", "Hair"];
+// var randomAdjectieves = ["Smelly", "Boring", "Stupid"];
+// var randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
+
+// var randomBodePart = randomBodeParts[Math.floor(Math.random() * 3)];
+// var randomAdjectieve = randomAdjectieves[Math.floor(Math.random() * 3)];
+// var randomWord = randomWords[Math.floor(Math.random() * 5)];
+// var randomInsult =
+//   "Your " +
+//   randomBodePart +
+//   " is like a " +
+//   randomAdjectieve +
+//   " " +
+//   randomWord +
+//   "!!!";
+
+// console.log(randomInsult);
+
+// var randomBodeParts = ["Face", "Nose", "Hair"];
+// var randomAdjectieves = ["Smelly", "Boring", "Stupid"];
+// var randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
+
+// var randomBodePart =
+//   randomBodeParts[Math.floor(Math.random() * randomBodeParts.length)];
+// var randomAdjectieve =
+//   randomAdjectieves[Math.floor(Math.random() * randomAdjectieves.length)];
+
+// var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+// var randomInsult =
+//   "Your " +
+//   randomBodePart +
+//   " is like a " +
+//   randomAdjectieve +
+//   " " +
+//   randomWord +
+//   "!!!";
+
+// console.log(randomInsult);
+
+// var randomBodeParts = ["Beard", "Face", "Hair"];
+// var randomAdjectieves = ["Sticky", "Ugly", "Smelly"];
+// var randomAnimals = ["Horse", "Iguana", "Giraffe", "Monkey"];
+// var randomAnimalsParts = ["Ass", "Leg", "Nose", "Fur"];
+
+// var randomBodePart =
+//   randomBodeParts[Math.floor(Math.random() * randomBodeParts.length)];
+// var randomAdjectieve =
+//   randomAdjectieves[Math.floor(Math.random() * randomAdjectieves.length)];
+// var randomAnimal =
+//   randomAnimals[Math.floor(Math.random() * randomAnimals.length)];
+// var randomAnimalsPart =
+//   randomAnimalsParts[Math.floor(Math.random() * randomAnimalsParts.length)];
+
+// var randomResult = [
+//   "Your",
+//   randomBodePart,
+//   "is more",
+//   randomAdjectieve,
+//   "than a",
+//   randomAnimal,
+//   "`s",
+//   randomAnimalsPart,
+// ].join(" ");
+
+// console.log(randomResult);
+
+// var cat = {
+//   legs: 3,
+//   ["name"]: "Harmony PHilips Trump",
+//   color: "yellow",
+// };
+
+// console.log(cat.name);
+
+// var dog = {
+//   name: "Pancake",
+//   age: 6,
+//   color: "white",
+//   bark: "Yip yap yupy",
+// };
+
+// console.log(Object.keys(dog));
+// console.log(dog);
+
+// var cat = {};
+// cat["Legs"] = 3;
+// cat["name"] = "Harmony";
+// cat["color"] = "red"
+
+// console.log(cat)
+
+// var anna ={ name: "Anna", age: 11, luckyNumbers: [2, 4, 8, 16]};
+// var dave ={ name: "Dave", age: 5, luckyNumbers: [3, 9, 40]};
+// var kate ={ name: "Kate", age: 9, luckyNumbers: [1, 2, 3]};
+// var friends = [anna, dave, kate];
+
+// console.log(friends[1])
+
+// var owedmoney = {};
+// owedmoney["Jimmy"] = 5;
+// owedmoney["Anna"] = 7;
+// owedmoney["Anna"] += 3;//owedmoney["Anna"] = owedmoney["Anna"] + 3
+
+// console.log(owedmoney["Anna"])
+// console.log(owedmoney["hut"])
+
+// var movies = {
+//     "Finding Nemo": {
+//         releaseDate: 2003,
+//         duration: 100,
+//         actors: ["Albert Brooks", "Ellen DeGenetates", "Alexader Gould"],
+//         format: "DVD"
+//     },
+//     "Star Wars": {
+//         releaseDate: 1983,
+//         duration: 134,
+//         actors: ["Mark Hamill", "Harington Ford", "Carrie Fisher"],
+//         format: "DVD"
+//     },
+//     "Harry Potter": {
+//         releaseDate: 2005,
+//         duration: 157,
+//         actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+//         format: "Blu-ray"
+//     }
+// }
+
+// console.log(Object.keys(movies))
+
+// var scores = {
+//   "Andrii": 0,
+//   "Zahar": 0,
+//   "Muhamed": 0,
+// };
+
+// scores["Zahar"] += 2
+
+// console.log(scores)
+
+// const name = "Huper";
+// console.log("Hello " + name);
+// if (name.length > 7) {
+//   console.log("Wooow, your name is really long"); //to work write more then 7 symbylles
+// } else {
+//   console.log("Nice name bro)");
+// }
+
+// var lemonChiken = false;
+// var beefWithSpecialSouce = false;
+// var sweetAndSourPork = false;
+
+// if (lemonChiken) {
+//     console.log ("Great! I'm having lemon chiken");
+// } else if (beefWithSpecialSouce) {
+//     console.log("I'm having beef");
+// } else if (sweetAndSourPork) {
+//     console.log("I'm having Pork");
+// } else {
+//     console.log("Okey, goodbye");
+// }
+
+// var name1 = "Nazar";
+// console.log("Hello " + name1);
+
+// var name2 = false;
+// var name3 = false;
+
+// if (name2) {
+//     console.log("Hello mother");
+// } else if (name3) {
+//     console.log("Hello father");
+// } else {
+//     console.log ("Hello stranger");
+// }
+
+// var sheepCount = 0;
+// while (sheepCount < 10) {
+//   console.log("I have counted " + sheepCount + " sheep");
+//   sheepCount++;
+// }
+// console.log("ZzzzZZZZZzzzzZz");
+
+// for (var sheepCount = 0; sheepCount < 10; sheepCount++) {
+//   console.log("I have count " + sheepCount + " sheep");
+// }
+// console.log("ZZzZZzzzZZ");
+
+// var timesToSayHello = 3;
+// for (var i = 0; i < timesToSayHello; i++) {
+//     console.log("Hello!")
+// }
+
+// var animals = ["Lion", "Tiger", "Polar bear", "Flamingo"];
+// for (var i = 0; i < animals.length; i++) {
+//   console.log("This zoo contains a " + animals[i] + ".");
+// }
+
+// var name0 = "Nickis";
+// for (var i = 0; i < name0.length; i++) {
+//   console.log("This name contains a letter " + name0[i]);
+// }
+
+// for (var x = 2; x < 10000; x = x * 2) {
+//   console.log(x);
+// }
+
+// var x = 3;
+// while (x < 10000) {
+//     console.log(x);
+//     x = x * 3;
+// }
+
+//1
+
+// var animals = ["Cat", "Fish", "Lemur", "Dragon"]
+// for(var i = 0; i < animals.length; i++ ) {
+//     animals[i] = "Awesome " + animals[i];
+//     //console.log("Awesome " + animals[i])
+// }
+// console.log(animals)
+
+//2
+
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// var randomString = "";
+// while (randomString.length < 6) {
+//   randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
+// }
+// console.log(randomString);
+
+//3
+
+// var input = "javascript is awesome";
+// var output = "";
+
+// for (var i = 0; i < input.length; i++) {
+//   var ch = input[i];
+
+//   if (ch === "o") {
+//     ch = "0";
+//   }
+//   if (ch === "a") ch = "4";
+//   if (ch === "i") ch = "1";
+//   if (ch === "e") ch = "3";
+
+//   output += ch;
+
+//   // if (ch == "j") input[i] = "A";
+// }
+
+// console.log(input.replaceAll("j", "2"));
+// console.log(output);
+
+// var outerFuntion = function (name) {
+//     console.log("Hello " + name + " World");
+// }
+
+// outerFuntion(2)
+
+// var drawCats = function (HowManyCatsDraw) {
+//     for (var i = 0; i < HowManyCatsDraw; i++) {
+//         console.log("=^.^=")
+//     }
+// }
+// drawCats(5)
+
+// var printMultipleemame = function (howManyTimes, whatToDraw) {
+//   for (var i = 1; i < howManyTimes + 1; i++) {
+//     console.log(i + " " + whatToDraw);
+//   }
+// };
+// printMultipleemame(5, "=^.^=");
+
+// console.log(5 + Math.floor(1.2345));
+
+// function double(number) {
+//   return number * 2;
+// }
+// console.log(double(7))
+
+// var pickRandomWord = function (words) {
+//   return words[Math.floor(Math.random() * words.length)];
+// };
+
+// generateRandomInsult = function () {
+//   var randomBodeParts = ["Face", "Nose", "Hair"];
+//   var randomAdjectieves = ["Smelly", "Boring", "Stupid"];
+//   var randomWords = ["Fly", "Monkey", "Stick", "Rat", "Marmot", "Pig"];
+
+//   var randomString =
+//     "Your " +
+//     pickRandomWord(randomBodeParts) +
+//     " is like a " +
+//     pickRandomWord(randomAdjectieves) +
+//     " " +
+//     pickRandomWord(randomWords) +
+//     "!!!";
+//   return randomString;
+// };
+// console.log(generateRandomInsult());
+
+// var fifthLetter = function (name) {
+//   if (name.length < 5) {
+//     return;
+//   }
+
+//   return "The fifth letter of your name is " + name[4] + "."
+// }
+// console.log(fifthLetter("Nazr"))
+
+// function multiply(number1, number2) {
+//   return number1 * number2;
+// }
+// function add(num1, num2) {
+//   return  num1 + num2;
+// }
+// console.log(add( multiply(36325, 9824), 777) )
+
+//npm start
